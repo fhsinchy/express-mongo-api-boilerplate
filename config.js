3c0aff462f844ab4d4e34c9f55ab91ecceabdd28
Farhan Hasin Chowdhury <mail@farhan.info>
2021-03-25 15:22:43 +0600

    Updated DB_COLLECTION > DB_DATABASE

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ module.exports = {
   db: {
     connectionMode: process.env.CONN_MODE,
     connectionString: {
-      dns: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_COLLECTION}`,
-      standard: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLLECTION}`,
+      dns: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`,
+      standard: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
     },
     connectionOptions: {
       useNewUrlParser: true,
