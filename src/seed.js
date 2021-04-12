cd573d822a56c88d64d8c90151fe72ed0ccf8bee
Farhan Hasin Chowdhury <mail@farhan.info>
2021-04-12 16:00:41 +0600

    Added comments in seeder

--- src/seed.js
+++ src/seed.js
@@ -10,31 +10,39 @@ const config = require('./config');
 
 const { seedDir } = config.seeder;
 
-mongoose.connect(config.db.connectionString[config.db.connectionMode], config.db.connectionOptions);
+mongoose.connect(config.db.connectionString, config.db.connectionOptions);
 
 mongoose.connection.on('connected', async () => {
+  // this list will be populated with the seed files.
   let files = [];
 
   try {
+    // read the seed directory and load the file list
     files = await fs.readdir(require('path').join(__dirname, seedDir));
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err);
   }
 
+  // for each file inside the file list
   for (const file of files) {
+    // requiring the file from the seed directory
     const seed = require(`./${seedDir}/${file}`);
 
+    // extracting the list of data to be seeded from the seed file
     const { data } = seed;
 
+    // creating an instance of the service used for seeding this model
     const service = new seed.Service(seed.Model);
 
     console.log(`seeding: ${file}`);
+    // for each entry inside the list of data
     for (const item of data) {
       console.dir(item);
       try {
+        // calling the create method from inside the service
         await service[seed.method](item);
-      } catch (error) {
-        console.error(error);
+      } catch (err) {
+        console.error(err);
       }
     }
     console.log('');
--- src/seeds/users.js
+++ src/seeds/users.js
@@ -1,7 +1,11 @@
 module.exports = {
+  // model to be used for seeding data
   Model: require('../auth/models/User'),
+  // service to be used for business logic
   Service: require('../auth/services/auth'),
+  // method used for creating new records
   method: 'signup',
+  // list of data to be seeded
   data: [
     {
       name: 'Farhan Hasin Chowdhury',
