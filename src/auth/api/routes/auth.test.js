2f2c466f91f8aab2445ce3220c3e2618dcd01183
Farhan Hasin Chowdhury <mail@farhan.info>
2021-04-12 14:09:10 +0600

    Added tests for validation errors

--- src/auth/api/routes/auth.test.js
+++ src/auth/api/routes/auth.test.js
@@ -53,6 +53,47 @@ describe('POST /auth/register', () => {
     expect(response.body.data.user).toHaveProperty('name', user.name);
     expect(response.body.data.user).toHaveProperty('email', user.email);
   });
+
+  test('Rejects unprocessable requests with proper error messages', async () => {
+    let response;
+    let errObj;
+
+    const userWithoutName = {
+      email: 'mail@farhan.info',
+      password: 'secret',
+    };
+    
+    response = await request(app).post('/auth/register').send(userWithoutName);
+    errObj = JSON.parse(response.error.text);
+
+    expect(response.status).toBe(400);
+    expect(errObj.status).toBe('fail');
+    expect(errObj.message).toBe('"name" is required');
+
+    const userWithoutEmail = {
+      name: 'Farhan Hasin Chowdhury',
+      password: 'secret',
+    };
+    
+    response = await request(app).post('/auth/register').send(userWithoutEmail);
+    errObj = JSON.parse(response.error.text);
+
+    expect(response.status).toBe(400);
+    expect(errObj.status).toBe('fail');
+    expect(errObj.message).toBe('"email" is required');
+
+    const userWithoutPassword = {
+      name: 'Farhan Hasin Chowdhury',
+      email: 'mail@farhan.info',
+    };
+    
+    response = await request(app).post('/auth/register').send(userWithoutPassword);
+    errObj = JSON.parse(response.error.text);
+
+    expect(response.status).toBe(400);
+    expect(errObj.status).toBe('fail');
+    expect(errObj.message).toBe('"password" is required');
+  });
 });
 
 describe('POST /auth/login', () => {
@@ -75,6 +116,33 @@ describe('POST /auth/login', () => {
       }),
     );
     expect(response.headers['set-cookie'].shift()).toContain('refreshToken');
+  });
+
+  test('Rejects unprocessable requests with proper error messages', async () => {
+    let response;
+    let errObj;
+
+    const userWithoutEmail = {
+      password: 'secret',
+    };
+    
+    response = await request(app).post('/auth/login').send(userWithoutEmail);
+    errObj = JSON.parse(response.error.text);
+
+    expect(response.status).toBe(400);
+    expect(errObj.status).toBe('fail');
+    expect(errObj.message).toBe('"email" is required');
+
+    const userWithoutPassword = {
+      email: 'mail@farhan.info',
+    };
+    
+    response = await request(app).post('/auth/login').send(userWithoutPassword);
+    errObj = JSON.parse(response.error.text);
+
+    expect(response.status).toBe(400);
+    expect(errObj.status).toBe('fail');
+    expect(errObj.message).toBe('"password" is required');
   });
 });
 
